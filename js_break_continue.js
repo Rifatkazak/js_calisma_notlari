cd21d86498c3b484b3693dade04a5ea0b18cec24
Rifat <rifat.kazak@skysens.io>
2021-12-26 01:32:53 +0300

    added comment

--- js_break_continue.js
+++ js_break_continue.js
@@ -3,13 +3,21 @@
 
 //şu olana kadar işleme devam et  işlem gerçekleşince durdur.. break
 
+{/* break ifadesi içinde bulunduğu döngüyü sonlandırmak diğer bir deyişle döngüden çıkılması hizmetini sunar. 
+    Bu hususta öneminin yeniden vurgulanması gereken nokta "break" 
+    ifadesinin sadece kendine en yakın yani içinde bulunduğu döngü içerisinde geçerli oluşudur.---->
+ İç içe döngüler örneğinde daha net anlaşılacaktır.*/}
+
 for(let counter = 0 ; counter < 10 ; i++) {
     console.log(counter)
     if (counter === 5) {
         break // conter 5 olunca çıkar o yüzden 5 e kadar yazdırır 5 i yazdırmaz 
     }
 }
-
+{/*
+Continue ifadesi ise bulunduğu döngü içinde o anki çalışacak olan devir işlemini pas geçerek bir sonraki devir işlemini başlatır. 
+Yine burada da vurgulanması gereken nokta "continue" ifadesinin sadece kendine en yakın yani içerisinde bulunduğu döngü içinde geçerli oluşudur.
+*/}
 for(let counter = 0 ; counter < 10 ; i++) {
     
     if (counter === 5) {
