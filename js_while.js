9b7b39cc68f568dff6f0b5ab1e3d8b3417f204fb
Rifat <rifat.kazak@skysens.io>
2021-12-26 01:37:29 +0300

    added while info

---
+++ js_while.js
@@ -0,0 +1,19 @@
+
+
+// while döngüsü
+// bir şey olana kadar
+
+let counter = 0 ;
+
+while(counter=10) {
+    console.log(counter)
+    counter++
+}
+
+// kullanıcının bilgisini alana kadar 
+
+let userName = "";
+
+while(userName == "") {
+  userName = prompt("İsimi giriniz") // isim girene kadar alır..
+}
