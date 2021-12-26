78f898aa094b7feb832c3d0e5030d9e8d995c229
Rifat <rifat.kazak@skysens.io>
2021-12-26 16:01:42 +0300

    filter added

---
+++ js_filter.js
@@ -0,0 +1,5 @@
+// liste içerisindeki öğeler üzerinde filtre uygulayıp yeni liste oluşturmak için .
+
+const PRODUCTS = ["APPLE", "SAMSUNG", "MİCROPHONE"]
+
+//Bir Array metodu olan filter(), dizi elemanlarını döngüye alıp, istediğimiz koşula sahip olanlarla yeni bir dizi oluşturmak için kullanılır.
